49a9e97ca92e36cc037bc207cf0b3cab12fa103a
anhminh0311 <163004032+anhminh0311@users.noreply.github.com>
2024-08-04 15:15:38 +0700

    Update Page.kt

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -11,18 +11,18 @@ data class Page(
 
 val pages = listOf<Page>(
     Page(
-        title = "Lorem ipsum is simply dummy",
-        description = "Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        title = "Welcome to my News app",
+        description = "Place you can update about everything from finance",
         image = R.drawable.onboarding1
     ),
     Page(
-        title ="Lorem ipsum is simply dummy",
-        description ="Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        title ="Welcome to my News app",
+        description ="To travel and news around the world",
         image = R.drawable.onboarding2
     ),
     Page(
-        title ="Lorem ipsum is simply dummy",
-        description ="Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        title ="Welcome to my News app",
+        description ="Or even discovery new dish and how to make it",
         image = R.drawable.onboarding3
     ),
 )
